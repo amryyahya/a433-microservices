8cee81bee2609f5ca486096a8215fc84a23bd89a
amryyahya <amryyahya3701@gmail.com>
2023-11-03 11:04:18 +0700

    add script to build image

---
+++ build_push_image_karsajobs_ui.sh
@@ -0,0 +1,22 @@
+# Membuat variabel untuk nama image
+IMAGE_NAME="karsajobs-ui"
+IMAGE_TAG="latest"
+
+# Membuat variabel username github packages
+GITHUB_USERNAME="amryyahya"
+
+# Membuat Docker image dari Dockerfile
+docker build -t "$IMAGE_NAME:$IMAGE_TAG" .
+
+# Melihat daftar image di lokal
+docker images
+
+# Mengubah nama image sesuai dengan format GitHub Packages
+NEW_IMAGE_NAME="ghcr.io/$GITHUB_USERNAME/$IMAGE_NAME:$IMAGE_TAG"
+docker tag "$IMAGE_NAME:$IMAGE_TAG" "$NEW_IMAGE_NAME"
+
+# Login ke GitHub Packages Docker Registry using Personal Access Token
+echo $GITHUB_PAT | docker login ghcr.io -u $GITHUB_USERNAME --password-stdin
+
+# Mengunggah image ke GitHub Packages Docker Registry
+docker push "$NEW_IMAGE_NAME"
